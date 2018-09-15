4f5431d7c5d700e0b948717a87ba21d3417b78d7
maxim199921 <maxim199921@rambler.ru>
2018-09-15 10:29:07 +0300

    homework4_2 finish

--- script/homeworks/4/homework4_2.js
+++ script/homeworks/4/homework4_2.js
@@ -24,9 +24,9 @@ class Person {
 }
 
 class Boy extends Person { // Базовый класс
-    constructor(man) { // Конструктор
-        super();
-        this.man = man; // Создание и инициализация поля
+    constructor(name) { // Конструктор
+        super(name);
+        this.man = name; // Создание и инициализация поля
     }
 
     getMarry(other) {
@@ -41,9 +41,9 @@ class Boy extends Person { // Базовый класс
 }
 
 class Girl extends Person { // Базовый класс
-    constructor(woman) { // Конструктор
-        super();
-        this.man = woman; // Создание и инициализация поля
+    constructor(name) { // Конструктор
+        super(name);
+        this.woman = name; // Создание и инициализация поля
     }
 
     getMarry(other) {
