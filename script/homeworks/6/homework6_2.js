b1aa71432e2f3f5c1626359192d3a148bd18fe86
maxim199921 <maxim199921@rambler.ru>
2018-09-18 12:48:31 +0300

    homework6_2 finish

---
+++ script/homeworks/6/homework6_2.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function randomDiap(n,m) {
+    return Math.floor(Math.random()*(m-n+1))+n;
+}
+
+function mood() {
+    var colors=['красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый', 'новый цвет' ];
+
+    var obj = {};
+
+    console.log( 'цветов: ' + colors.length );
+
+    for (var i = 0; i < colors.length; i++) {
+        var n=randomDiap(0, colors.length - 1);
+        var colorName = colors[n];
+        obj[colorName] = colors;
+    }
+
+    return Object.keys(obj);
+
+    // for ( var i=0; i<=colors.length; i++ ) {
+    //     var n=randomDiap(0,colors.length - 1);
+    //     var colorName=colors[n];
+    //
+    // }
+    // return colorName;
+}
+
+
+
+console.log(mood());
+
