ba6f79106b088a5cf04243de090d8037a3f1c1a8
maxim199921 <maxim199921@rambler.ru>
2018-09-24 17:28:53 +0300

    8.2 finish

---
+++ script/homeworks/8/homework8_2/homework8_2.js
@@ -0,0 +1,28 @@
+// Как разобрать строку с форматными символами и построить DOM
+// на ее базе
+function formatFromCodes(element, formatString) {
+    let s = formatString.trim();
+    console.log(s);
+    let parts = s.split('[B]').filter(x => !!x);
+    console.log(parts);
+    let startsFromBoldEm = s.indexOf('[B]') === 0;
+    for (let i = 0; i < parts.length; i++) {
+        if (i % 2 === 0 && startsFromBoldEm) {
+            const bold = document.createElement('strong');
+            bold.textContent = parts[i];
+            element.appendChild(bold);
+        }   else   if (i / 3 === 1 || i / 7 === 1 && startsFromBoldEm) {
+            const em = document.createElement('em');
+            em.textContent = parts[i];
+            element.appendChild(em);
+            } else {
+            const text = document.createTextNode(parts[i]);
+            element.appendChild(text);
+        }
+    }
+
+}
+
+formatFromCodes(
+    document.body,
+    "[B]это жирный текст[B] а это обычный [B], а это опять жирный текст[B], а это курсив, [B]это жирный текст[B] а это обычный [B], а это опять жирный текст[B], а это курсив");
