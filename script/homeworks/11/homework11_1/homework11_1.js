9b2d06541d37374011677d77fb11ba1e8836b73d
maxim199921 <maxim199921@rambler.ru>
2018-10-02 12:36:55 +0300

    11._1 fin

--- script/homeworks/11/homework11_1/homework11_1.js
+++ script/homeworks/11/homework11_1/homework11_1.js
@@ -1,6 +1,7 @@
 const form = document.forms["testForm"];
+form.addEventListener('submit', sendForm);           // - повесил событе не на кнопку а на форму, через submit.
 
-document.getElementById("good").addEventListener('click', sendForm);
+// document.getElementById("good").addEventListener('click', sendForm);
 
 function deleteError(container) {
     if (container.lastChild.className === "error-message") {
@@ -8,7 +9,16 @@ function deleteError(container) {
     }
 }
 
-function sendForm() {
+function error(item, errorMessage) {                  // - вынес в отдельную функцию ошибку
+    let element = document.createElement('span');
+    element.innerHTML = errorMessage;
+    element.className = "error-message";
+    item.parentNode.appendChild(element);
+    item.focus();
+}
+
+function sendForm(e) {
+    e.preventDefault();                                // - отменил действие отправки формы по умолчанию
 
     deleteError(form.url.parentNode);
     deleteError(form.title.parentNode);
@@ -18,70 +28,43 @@ function sendForm() {
     deleteError(form.rubric.parentNode);
     deleteError(form.author.parentNode);
     deleteError(form.article.parentNode);
+    deleteError(form.comments.parentNode);
 
 
     if (form.article.value.length === 0) {
-        let element = document.createElement('span');
-        element.innerHTML = `<br> нет текста`;
-        element.className = "error-message";
-        form.article.parentNode.appendChild(element);
-        form.article.focus();
+        error(form.article, `<br> нет текста`);
+    }
+
+    if (form.comments.checked === false) {
+        error(form.comments, `необходимо ваше разрешение!!!`);
     }
 
     if (form.rubric.value.length === 0) {
-        let element = document.createElement('span');
-        element.innerHTML = "Введите корректно";
-        element.className = "error-message";
-        form.rubric.parentNode.appendChild(element);
-        form.rubric.focus();
+        error(form.rubric, `введите данные!!!`);
     }
 
     if (form.mail.value.length === 0) {
-        let element = document.createElement('span');
-        element.innerHTML = "Введите корректно";
-        element.className = "error-message";
-        form.mail.parentNode.appendChild(element);
-        form.mail.focus();
+        error(form.mail, `введите данные!!!`);
     }
 
     if (form.persons.value.length === 0) {
-        let element = document.createElement('span');
-        element.innerHTML = "Введите корректно";
-        element.className = "error-message";
-        form.persons.parentNode.appendChild(element);
-        form.persons.focus();
+        error(form.persons, `введите данные!!!`);
     }
 
     if (form.startdate.value.length === 0) {
-        let element = document.createElement('span');
-        element.innerHTML = "Введите корректно";
-        element.className = "error-message";
-        form.startdate.parentNode.appendChild(element);
-        form.startdate.focus();
+        error(form.startdate, `введите данные!!!`);
     }
 
     if (form.url.value.length === 0) {
-        let element = document.createElement('span');
-        element.innerHTML = "Введите корректно";
-        element.className = "error-message";
-        form.url.parentNode.appendChild(element);
-        form.url.focus();
+        error(form.url, `введите данные!!!`);
     }
 
     if (form.title.value.length === 0) {
-        let element = document.createElement('span');
-        element.innerHTML = "Введите корректно";
-        element.className = "error-message";
-        form.title.parentNode.appendChild(element);
-        form.title.focus();
+        error(form.title, `введите данные!!!`);
     }
 
     if (form.author.value.length === 0) {
-        let element = document.createElement('span');
-        element.innerHTML = "Введите корректно";
-        element.className = "error-message";
-        form.author.parentNode.appendChild(element);
-        form.author.focus();
+        error(form.author, `введите данные!!!`);
     }
 
 
@@ -92,68 +75,9 @@ function sendForm() {
         && form.persons.value.length !== 0
         && form.mail.value.length !== 0
         && form.rubric.value.length !== 0
+        && form.comments.checked === true
         && form.article.value.length !== 0) {
         form.submit();
     }
-    // switch (form) {
-    //
-    //     case form.article.value.length === 0 :
-    //         let element1 = document.createElement('span');
-    //         element1.innerHTML = `<br> нет текста`;
-    //         form.article.parentNode.appendChild(element1);
-    //         form.article.focus();
-    //         break;
-    //
-    //     case form.rubric.value.length === 0 :
-    //         let element2 = document.createElement('span');
-    //         element2.innerHTML = "Введите корректно";
-    //         form.rubric.parentNode.appendChild(element2);
-    //         form.rubric.focus();
-    //         break;
-    //
-    //     case form.mail.value.length === 0 :
-    //         let element3 = document.createElement('span');
-    //         element3.innerHTML = "Введите корректно";
-    //         form.mail.parentNode.appendChild(element3);
-    //         form.mail.focus();
-    //         break;
-    //
-    //     case form.persons.value.length === 0 :
-    //         let element4 = document.createElement('span');
-    //         element4.innerHTML = "Введите корректно";
-    //         form.persons.parentNode.appendChild(element4);
-    //         form.persons.focus();
-    //         break;
-    //
-    //     case form.startdate.value.length === 0 :
-    //         let element5 = document.createElement('span');
-    //         element5.innerHTML = "Введите корректно";
-    //         form.startdate.parentNode.appendChild(element5);
-    //         form.startdate.focus();
-    //         break;
-    //
-    //     case form.url.value.length === 0 :
-    //         let element6 = document.createElement('span');
-    //         element6.innerHTML = "Введите корректно";
-    //         form.url.parentNode.appendChild(element6);
-    //         form.url.focus();
-    //         break;
-    //
-    //     case form.title.value.length === 0 :
-    //         let element7 = document.createElement('span');
-    //         element7.innerHTML = "Введите корректно";
-    //         form.title.parentNode.appendChild(element7);
-    //         form.title.focus();
-    //         break;
-    //
-    //     case form.author.value.length === 0 :
-    //         let element8 = document.createElement('span');
-    //         element8.innerHTML = "Введите корректно";
-    //         form.author.parentNode.appendChild(element8);
-    //         form.author.focus();
-    //         break;
-    //
-    //     default: alert("error");
-    // }
 }
 
